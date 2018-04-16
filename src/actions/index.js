1d5951ebb7e3bc85b36d67482d8427a1ef833ebc
Joseph Lin <josephlin55555@gmail.com>
2018-04-16 18:15:35 -0400

    [adjust] instructions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,7 +3,7 @@ import axios from './../http';
 
 // https://swapi.co/api/people/
 export const getPerson = () => {
-    // delete inside here and replace with actual API request using axios
+    // delete this timeout and replace with actual API request using axios
     setTimeout(() => {
 
         store.dispatch({
